78e80e04b9bf3debb7e7997131c6dd6b8fbd37e0
hakeemnazri <mhakeemnazri@gmail.com>
2025-08-11 22:21:38 +0800

    validation on model level

--- src/main/java/com/spring_boot/ecommerce/model/Product.java
+++ src/main/java/com/spring_boot/ecommerce/model/Product.java
@@ -1,6 +1,8 @@
 package com.spring_boot.ecommerce.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,7 +15,13 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long productId;
+
+    @NotBlank
+    @Size(min = 3, message = "Product must at least be 3 characters")
     private String productName;
+
+    @NotBlank
+    @Size(min = 10, message = "Product must at least be 10 characters")
     private String description;
     private String image;
     private Integer quantity;
