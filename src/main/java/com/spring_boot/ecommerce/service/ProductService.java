f1b099a2cf535ecec50e36ab2679ac8c80fd1376
hakeemnazri <mhakeemnazri@gmail.com>
2025-08-11 21:25:13 +0800

    fix request body to productDTO at create product controller

--- src/main/java/com/spring_boot/ecommerce/service/ProductService.java
+++ src/main/java/com/spring_boot/ecommerce/service/ProductService.java
@@ -7,7 +7,7 @@ import com.spring_boot.ecommerce.payload.ProductResponse;
 import java.util.List;
 
 public interface ProductService {
-    ProductDTO addProduct(Long categoryId, Product product);
+    ProductDTO addProduct(Long categoryId, ProductDTO productDTO);
 
     ProductResponse getAllProducts();
 
